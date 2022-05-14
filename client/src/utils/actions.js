bd2285f3ac4a3dc11acfbad9cbf1e4d1b880f69b
Adrian <adrianhenry2115@gmail.com>
2022-05-14 10:05:24 -0400

    reducers and useReducer hook learned but not finished

---
+++ client/src/utils/actions.js
@@ -0,0 +1,6 @@
+// Store data for products by Apollo in global state for offline capabilities
+export const UPDATE_PRODUCTS = "UPDATE_PRODUCTS";
+// same as products
+export const UPDATE_CATEGORIES = "UPDATE_CATEGORIES";
+// select category from categories state and display products
+export const UPDATE_CURRENT_CATEGORY = "UPDATE_CURRENT_CATEGORY";
